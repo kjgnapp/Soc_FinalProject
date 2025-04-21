7b311b061a3da27ffe3771dcc6ff7851b9c360f7
benjihern <bhernandez2@ufl.edu>
2025-04-20 20:15:51 -0400

    task 2 files again

---
+++ project_2.xpr/project_2/project_2.gen/sources_1/bd/design_1/ip/design_1_apb_led_ctl_0_3/design_1_apb_led_ctl_0_3_sim_netlist.vhdl
@@ -0,0 +1,504 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+-- Date        : Mon Feb 24 02:08:13 2025
+-- Host        : bipe_surface_v2 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode funcsim
+--               c:/VivadoProjects/project_2/project_2.gen/sources_1/bd/design_1/ip/design_1_apb_led_ctl_0_3/design_1_apb_led_ctl_0_3_sim_netlist.vhdl
+-- Design      : design_1_apb_led_ctl_0_3
+-- Purpose     : This VHDL netlist is a functional simulation representation of the design and should not be modified or
+--               synthesized. This netlist cannot be used for SDF annotated simulation.
+-- Device      : xc7s50csga324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_apb_led_ctl_0_3_led_ctl is
+  port (
+    rgbB : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    rgbA : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    led : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    prdata : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    pwdata : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    rst : in STD_LOGIC;
+    clk : in STD_LOGIC;
+    pwrite : in STD_LOGIC;
+    penable : in STD_LOGIC;
+    psel : in STD_LOGIC;
+    paddr : in STD_LOGIC_VECTOR ( 1 downto 0 )
+  );
+  attribute ORIG_REF_NAME : string;
+  attribute ORIG_REF_NAME of design_1_apb_led_ctl_0_3_led_ctl : entity is "led_ctl";
+end design_1_apb_led_ctl_0_3_led_ctl;
+
+architecture STRUCTURE of design_1_apb_led_ctl_0_3_led_ctl is
+  signal \^led\ : STD_LOGIC_VECTOR ( 7 downto 0 );
+  signal \led_data0__0\ : STD_LOGIC;
+  signal \^rgba\ : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal \rgbA_data0__0\ : STD_LOGIC;
+  signal \rgbA_data[0]_i_1_n_0\ : STD_LOGIC;
+  signal \rgbA_data[1]_i_1_n_0\ : STD_LOGIC;
+  signal \rgbA_data[2]_i_1_n_0\ : STD_LOGIC;
+  signal \^rgbb\ : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal \rgbB_data0__0\ : STD_LOGIC;
+  signal \rgbB_data[0]_i_1_n_0\ : STD_LOGIC;
+  signal \rgbB_data[1]_i_1_n_0\ : STD_LOGIC;
+  signal \rgbB_data[2]_i_1_n_0\ : STD_LOGIC;
+  attribute SOFT_HLUTNM : string;
+  attribute SOFT_HLUTNM of \prdata[3]_INST_0\ : label is "soft_lutpair4";
+  attribute SOFT_HLUTNM of \prdata[4]_INST_0\ : label is "soft_lutpair4";
+  attribute SOFT_HLUTNM of \prdata[5]_INST_0\ : label is "soft_lutpair5";
+  attribute SOFT_HLUTNM of \prdata[6]_INST_0\ : label is "soft_lutpair5";
+  attribute SOFT_HLUTNM of rgbA_data0 : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of \rgbA_data[0]_i_1\ : label is "soft_lutpair3";
+  attribute SOFT_HLUTNM of \rgbA_data[1]_i_1\ : label is "soft_lutpair2";
+  attribute SOFT_HLUTNM of \rgbA_data[2]_i_1\ : label is "soft_lutpair1";
+  attribute SOFT_HLUTNM of rgbB_data0 : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of \rgbB_data[0]_i_1\ : label is "soft_lutpair3";
+  attribute SOFT_HLUTNM of \rgbB_data[1]_i_1\ : label is "soft_lutpair2";
+  attribute SOFT_HLUTNM of \rgbB_data[2]_i_1\ : label is "soft_lutpair1";
+begin
+  led(7 downto 0) <= \^led\(7 downto 0);
+  rgbA(2 downto 0) <= \^rgba\(2 downto 0);
+  rgbB(2 downto 0) <= \^rgbb\(2 downto 0);
+led_data0: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"00000080"
+    )
+        port map (
+      I0 => pwrite,
+      I1 => penable,
+      I2 => psel,
+      I3 => paddr(1),
+      I4 => paddr(0),
+      O => \led_data0__0\
+    );
+\led_data_reg[0]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(0),
+      Q => \^led\(0),
+      R => rst
+    );
+\led_data_reg[1]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(1),
+      Q => \^led\(1),
+      R => rst
+    );
+\led_data_reg[2]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(2),
+      Q => \^led\(2),
+      R => rst
+    );
+\led_data_reg[3]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(3),
+      Q => \^led\(3),
+      R => rst
+    );
+\led_data_reg[4]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(4),
+      Q => \^led\(4),
+      R => rst
+    );
+\led_data_reg[5]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(5),
+      Q => \^led\(5),
+      R => rst
+    );
+\led_data_reg[6]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(6),
+      Q => \^led\(6),
+      R => rst
+    );
+\led_data_reg[7]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => \led_data0__0\,
+      D => pwdata(7),
+      Q => \^led\(7),
+      R => rst
+    );
+\prdata[0]_INST_0\: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"30BB3088"
+    )
+        port map (
+      I0 => \^rgba\(0),
+      I1 => paddr(0),
+      I2 => \^rgbb\(0),
+      I3 => paddr(1),
+      I4 => \^led\(0),
+      O => prdata(0)
+    );
+\prdata[1]_INST_0\: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"30BB3088"
+    )
+        port map (
+      I0 => \^rgba\(1),
+      I1 => paddr(0),
+      I2 => \^rgbb\(1),
+      I3 => paddr(1),
+      I4 => \^led\(1),
+      O => prdata(1)
+    );
+\prdata[2]_INST_0\: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"30BB3088"
+    )
+        port map (
+      I0 => \^rgba\(2),
+      I1 => paddr(0),
+      I2 => \^rgbb\(2),
+      I3 => paddr(1),
+      I4 => \^led\(2),
+      O => prdata(2)
+    );
+\prdata[3]_INST_0\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"04"
+    )
+        port map (
+      I0 => paddr(0),
+      I1 => \^led\(3),
+      I2 => paddr(1),
+      O => prdata(3)
+    );
+\prdata[4]_INST_0\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"04"
+    )
+        port map (
+      I0 => paddr(0),
+      I1 => \^led\(4),
+      I2 => paddr(1),
+      O => prdata(4)
+    );
+\prdata[5]_INST_0\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"04"
+    )
+        port map (
+      I0 => paddr(0),
+      I1 => \^led\(5),
+      I2 => paddr(1),
+      O => prdata(5)
+    );
+\prdata[6]_INST_0\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"04"
+    )
+        port map (
+      I0 => paddr(0),
+      I1 => \^led\(6),
+      I2 => paddr(1),
+      O => prdata(6)
+    );
+\prdata[7]_INST_0\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"04"
+    )
+        port map (
+      I0 => paddr(0),
+      I1 => \^led\(7),
+      I2 => paddr(1),
+      O => prdata(7)
+    );
+rgbA_data0: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"00800000"
+    )
+        port map (
+      I0 => pwrite,
+      I1 => penable,
+      I2 => psel,
+      I3 => paddr(1),
+      I4 => paddr(0),
+      O => \rgbA_data0__0\
+    );
+\rgbA_data[0]_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"B8"
+    )
+        port map (
+      I0 => pwdata(0),
+      I1 => \rgbA_data0__0\,
+      I2 => \^rgba\(0),
+      O => \rgbA_data[0]_i_1_n_0\
+    );
+\rgbA_data[1]_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"B8"
+    )
+        port map (
+      I0 => pwdata(1),
+      I1 => \rgbA_data0__0\,
+      I2 => \^rgba\(1),
+      O => \rgbA_data[1]_i_1_n_0\
+    );
+\rgbA_data[2]_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"B8"
+    )
+        port map (
+      I0 => pwdata(2),
+      I1 => \rgbA_data0__0\,
+      I2 => \^rgba\(2),
+      O => \rgbA_data[2]_i_1_n_0\
+    );
+\rgbA_data_reg[0]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => '1',
+      D => \rgbA_data[0]_i_1_n_0\,
+      Q => \^rgba\(0),
+      R => rst
+    );
+\rgbA_data_reg[1]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => '1',
+      D => \rgbA_data[1]_i_1_n_0\,
+      Q => \^rgba\(1),
+      R => rst
+    );
+\rgbA_data_reg[2]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => '1',
+      D => \rgbA_data[2]_i_1_n_0\,
+      Q => \^rgba\(2),
+      R => rst
+    );
+rgbB_data0: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"00800000"
+    )
+        port map (
+      I0 => pwrite,
+      I1 => penable,
+      I2 => psel,
+      I3 => paddr(0),
+      I4 => paddr(1),
+      O => \rgbB_data0__0\
+    );
+\rgbB_data[0]_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"B8"
+    )
+        port map (
+      I0 => pwdata(0),
+      I1 => \rgbB_data0__0\,
+      I2 => \^rgbb\(0),
+      O => \rgbB_data[0]_i_1_n_0\
+    );
+\rgbB_data[1]_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"B8"
+    )
+        port map (
+      I0 => pwdata(1),
+      I1 => \rgbB_data0__0\,
+      I2 => \^rgbb\(1),
+      O => \rgbB_data[1]_i_1_n_0\
+    );
+\rgbB_data[2]_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"B8"
+    )
+        port map (
+      I0 => pwdata(2),
+      I1 => \rgbB_data0__0\,
+      I2 => \^rgbb\(2),
+      O => \rgbB_data[2]_i_1_n_0\
+    );
+\rgbB_data_reg[0]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => '1',
+      D => \rgbB_data[0]_i_1_n_0\,
+      Q => \^rgbb\(0),
+      R => rst
+    );
+\rgbB_data_reg[1]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => '1',
+      D => \rgbB_data[1]_i_1_n_0\,
+      Q => \^rgbb\(1),
+      R => rst
+    );
+\rgbB_data_reg[2]\: unisim.vcomponents.FDRE
+     port map (
+      C => clk,
+      CE => '1',
+      D => \rgbB_data[2]_i_1_n_0\,
+      Q => \^rgbb\(2),
+      R => rst
+    );
+end STRUCTURE;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_apb_led_ctl_0_3_apb_led_ctl is
+  port (
+    rgbB : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    rgbA : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    led : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    prdata : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    pwdata : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    rst : in STD_LOGIC;
+    clk : in STD_LOGIC;
+    pwrite : in STD_LOGIC;
+    penable : in STD_LOGIC;
+    psel : in STD_LOGIC;
+    paddr : in STD_LOGIC_VECTOR ( 1 downto 0 )
+  );
+  attribute ORIG_REF_NAME : string;
+  attribute ORIG_REF_NAME of design_1_apb_led_ctl_0_3_apb_led_ctl : entity is "apb_led_ctl";
+end design_1_apb_led_ctl_0_3_apb_led_ctl;
+
+architecture STRUCTURE of design_1_apb_led_ctl_0_3_apb_led_ctl is
+begin
+led_inst: entity work.design_1_apb_led_ctl_0_3_led_ctl
+     port map (
+      clk => clk,
+      led(7 downto 0) => led(7 downto 0),
+      paddr(1 downto 0) => paddr(1 downto 0),
+      penable => penable,
+      prdata(7 downto 0) => prdata(7 downto 0),
+      psel => psel,
+      pwdata(7 downto 0) => pwdata(7 downto 0),
+      pwrite => pwrite,
+      rgbA(2 downto 0) => rgbA(2 downto 0),
+      rgbB(2 downto 0) => rgbB(2 downto 0),
+      rst => rst
+    );
+end STRUCTURE;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_apb_led_ctl_0_3 is
+  port (
+    rst : in STD_LOGIC;
+    clk : in STD_LOGIC;
+    paddr : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    psel : in STD_LOGIC;
+    penable : in STD_LOGIC;
+    pwrite : in STD_LOGIC;
+    pready : out STD_LOGIC;
+    pslverr : out STD_LOGIC;
+    pwdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    prdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    led : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    rgbA : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    rgbB : out STD_LOGIC_VECTOR ( 2 downto 0 )
+  );
+  attribute NotValidForBitStream : boolean;
+  attribute NotValidForBitStream of design_1_apb_led_ctl_0_3 : entity is true;
+  attribute CHECK_LICENSE_TYPE : string;
+  attribute CHECK_LICENSE_TYPE of design_1_apb_led_ctl_0_3 : entity is "design_1_apb_led_ctl_0_3,apb_led_ctl,{}";
+  attribute DowngradeIPIdentifiedWarnings : string;
+  attribute DowngradeIPIdentifiedWarnings of design_1_apb_led_ctl_0_3 : entity is "yes";
+  attribute IP_DEFINITION_SOURCE : string;
+  attribute IP_DEFINITION_SOURCE of design_1_apb_led_ctl_0_3 : entity is "package_project";
+  attribute X_CORE_INFO : string;
+  attribute X_CORE_INFO of design_1_apb_led_ctl_0_3 : entity is "apb_led_ctl,Vivado 2024.2";
+end design_1_apb_led_ctl_0_3;
+
+architecture STRUCTURE of design_1_apb_led_ctl_0_3 is
+  signal \<const0>\ : STD_LOGIC;
+  signal \<const1>\ : STD_LOGIC;
+  signal \^prdata\ : STD_LOGIC_VECTOR ( 7 downto 0 );
+  attribute X_INTERFACE_INFO : string;
+  attribute X_INTERFACE_INFO of clk : signal is "xilinx.com:signal:clock:1.0 clk CLK";
+  attribute X_INTERFACE_MODE : string;
+  attribute X_INTERFACE_MODE of clk : signal is "slave";
+  attribute X_INTERFACE_PARAMETER : string;
+  attribute X_INTERFACE_PARAMETER of clk : signal is "XIL_INTERFACENAME clk, ASSOCIATED_RESET rst, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, PHASE 0.0, CLK_DOMAIN /clk_wiz_1_clk_out1, INSERT_VIP 0";
+  attribute X_INTERFACE_INFO of penable : signal is "xilinx.com:interface:apb:1.0 interface_apb PENABLE";
+  attribute X_INTERFACE_INFO of pready : signal is "xilinx.com:interface:apb:1.0 interface_apb PREADY";
+  attribute X_INTERFACE_INFO of psel : signal is "xilinx.com:interface:apb:1.0 interface_apb PSEL";
+  attribute X_INTERFACE_INFO of pslverr : signal is "xilinx.com:interface:apb:1.0 interface_apb PSLVERR";
+  attribute X_INTERFACE_INFO of pwrite : signal is "xilinx.com:interface:apb:1.0 interface_apb PWRITE";
+  attribute X_INTERFACE_INFO of rst : signal is "xilinx.com:signal:reset:1.0 rst RST";
+  attribute X_INTERFACE_MODE of rst : signal is "slave";
+  attribute X_INTERFACE_PARAMETER of rst : signal is "XIL_INTERFACENAME rst, POLARITY ACTIVE_HIGH, INSERT_VIP 0";
+  attribute X_INTERFACE_INFO of paddr : signal is "xilinx.com:interface:apb:1.0 interface_apb PADDR";
+  attribute X_INTERFACE_MODE of paddr : signal is "slave";
+  attribute X_INTERFACE_INFO of prdata : signal is "xilinx.com:interface:apb:1.0 interface_apb PRDATA";
+  attribute X_INTERFACE_INFO of pwdata : signal is "xilinx.com:interface:apb:1.0 interface_apb PWDATA";
+begin
+  prdata(31) <= \<const0>\;
+  prdata(30) <= \<const0>\;
+  prdata(29) <= \<const0>\;
+  prdata(28) <= \<const0>\;
+  prdata(27) <= \<const0>\;
+  prdata(26) <= \<const0>\;
+  prdata(25) <= \<const0>\;
+  prdata(24) <= \<const0>\;
+  prdata(23) <= \<const0>\;
+  prdata(22) <= \<const0>\;
+  prdata(21) <= \<const0>\;
+  prdata(20) <= \<const0>\;
+  prdata(19) <= \<const0>\;
+  prdata(18) <= \<const0>\;
+  prdata(17) <= \<const0>\;
+  prdata(16) <= \<const0>\;
+  prdata(15) <= \<const0>\;
+  prdata(14) <= \<const0>\;
+  prdata(13) <= \<const0>\;
+  prdata(12) <= \<const0>\;
+  prdata(11) <= \<const0>\;
+  prdata(10) <= \<const0>\;
+  prdata(9) <= \<const0>\;
+  prdata(8) <= \<const0>\;
+  prdata(7 downto 0) <= \^prdata\(7 downto 0);
+  pready <= \<const1>\;
+  pslverr <= \<const0>\;
+GND: unisim.vcomponents.GND
+     port map (
+      G => \<const0>\
+    );
+VCC: unisim.vcomponents.VCC
+     port map (
+      P => \<const1>\
+    );
+inst: entity work.design_1_apb_led_ctl_0_3_apb_led_ctl
+     port map (
+      clk => clk,
+      led(7 downto 0) => led(7 downto 0),
+      paddr(1 downto 0) => paddr(3 downto 2),
+      penable => penable,
+      prdata(7 downto 0) => \^prdata\(7 downto 0),
+      psel => psel,
+      pwdata(7 downto 0) => pwdata(7 downto 0),
+      pwrite => pwrite,
+      rgbA(2 downto 0) => rgbA(2 downto 0),
+      rgbB(2 downto 0) => rgbB(2 downto 0),
+      rst => rst
+    );
+end STRUCTURE;
---
+++ project_2.xpr/project_2/project_2.ip_user_files/ip/myip_pong2_1/myip_pong2_1_stub.vhdl
@@ -0,0 +1,96 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+-- Date        : Sun Feb 23 21:33:57 2025
+-- Host        : bipe_surface_v2 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/VivadoProjects/project_2/project_2.gen/sources_1/ip/myip_pong2_1/myip_pong2_1_stub.vhdl
+-- Design      : myip_pong2_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s50csga324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity myip_pong2_1 is
+  Port ( 
+    s00_axi_aclk : in STD_LOGIC;
+    s00_axi_aresetn : in STD_LOGIC;
+    s00_axi_awaddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_awprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    s00_axi_awvalid : in STD_LOGIC;
+    s00_axi_awready : out STD_LOGIC;
+    s00_axi_wdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s00_axi_wstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_wvalid : in STD_LOGIC;
+    s00_axi_wready : out STD_LOGIC;
+    s00_axi_bresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s00_axi_bvalid : out STD_LOGIC;
+    s00_axi_bready : in STD_LOGIC;
+    s00_axi_araddr : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_arprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    s00_axi_arvalid : in STD_LOGIC;
+    s00_axi_arready : out STD_LOGIC;
+    s00_axi_rdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    s00_axi_rresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s00_axi_rvalid : out STD_LOGIC;
+    s00_axi_rready : in STD_LOGIC;
+    rst_0_port_out : in STD_LOGIC;
+    hdmi_tx_0_tmds_clk_n_port_out : out STD_LOGIC;
+    hdmi_tx_0_tmds_data_n_port_out : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    hdmi_tx_0_tmds_clk_p_port_out : out STD_LOGIC;
+    hdmi_tx_0_tmds_data_p_port_out : out STD_LOGIC_VECTOR ( 2 downto 0 )
+  );
+
+  attribute CHECK_LICENSE_TYPE : string;
+  attribute CHECK_LICENSE_TYPE of myip_pong2_1 : entity is "myip_pong2_1,myip_pong2,{}";
+  attribute core_generation_info : string;
+  attribute core_generation_info of myip_pong2_1 : entity is "myip_pong2_1,myip_pong2,{x_ipProduct=Vivado 2024.2,x_ipVendor=xilinx.com,x_ipLibrary=user,x_ipName=myip_pong2,x_ipVersion=1.0,x_ipCoreRevision=1,x_ipLanguage=VHDL,x_ipSimLanguage=MIXED,C_S00_AXI_DATA_WIDTH=32,C_S00_AXI_ADDR_WIDTH=4}";
+  attribute downgradeipidentifiedwarnings : string;
+  attribute downgradeipidentifiedwarnings of myip_pong2_1 : entity is "yes";
+  attribute ip_definition_source : string;
+  attribute ip_definition_source of myip_pong2_1 : entity is "package_project";
+end myip_pong2_1;
+
+architecture stub of myip_pong2_1 is
+  attribute syn_black_box : boolean;
+  attribute black_box_pad_pin : string;
+  attribute syn_black_box of stub : architecture is true;
+  attribute black_box_pad_pin of stub : architecture is "s00_axi_aclk,s00_axi_aresetn,s00_axi_awaddr[3:0],s00_axi_awprot[2:0],s00_axi_awvalid,s00_axi_awready,s00_axi_wdata[31:0],s00_axi_wstrb[3:0],s00_axi_wvalid,s00_axi_wready,s00_axi_bresp[1:0],s00_axi_bvalid,s00_axi_bready,s00_axi_araddr[3:0],s00_axi_arprot[2:0],s00_axi_arvalid,s00_axi_arready,s00_axi_rdata[31:0],s00_axi_rresp[1:0],s00_axi_rvalid,s00_axi_rready,rst_0_port_out,hdmi_tx_0_tmds_clk_n_port_out,hdmi_tx_0_tmds_data_n_port_out[2:0],hdmi_tx_0_tmds_clk_p_port_out,hdmi_tx_0_tmds_data_p_port_out[2:0]";
+  attribute x_interface_info : string;
+  attribute x_interface_info of s00_axi_aclk : signal is "xilinx.com:signal:clock:1.0 s00_axi_aclk CLK";
+  attribute x_interface_mode : string;
+  attribute x_interface_mode of s00_axi_aclk : signal is "slave s00_axi_aclk";
+  attribute x_interface_parameter : string;
+  attribute x_interface_parameter of s00_axi_aclk : signal is "XIL_INTERFACENAME s00_axi_aclk, ASSOCIATED_BUSIF s00_axi, ASSOCIATED_RESET s00_axi_aresetn, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, PHASE 0.0, INSERT_VIP 0";
+  attribute x_interface_info of s00_axi_aresetn : signal is "xilinx.com:signal:reset:1.0 s00_axi_aresetn RST";
+  attribute x_interface_mode of s00_axi_aresetn : signal is "slave s00_axi_aresetn";
+  attribute x_interface_parameter of s00_axi_aresetn : signal is "XIL_INTERFACENAME s00_axi_aresetn, POLARITY ACTIVE_LOW, INSERT_VIP 0";
+  attribute x_interface_info of s00_axi_awaddr : signal is "xilinx.com:interface:aximm:1.0 s00_axi AWADDR";
+  attribute x_interface_mode of s00_axi_awaddr : signal is "slave s00_axi";
+  attribute x_interface_parameter of s00_axi_awaddr : signal is "XIL_INTERFACENAME s00_axi, DATA_WIDTH 32, PROTOCOL AXI4LITE, FREQ_HZ 100000000, ID_WIDTH 0, ADDR_WIDTH 4, AWUSER_WIDTH 0, ARUSER_WIDTH 0, WUSER_WIDTH 0, RUSER_WIDTH 0, BUSER_WIDTH 0, READ_WRITE_MODE READ_WRITE, HAS_BURST 0, HAS_LOCK 0, HAS_PROT 1, HAS_CACHE 0, HAS_QOS 0, HAS_REGION 0, HAS_WSTRB 1, HAS_BRESP 1, HAS_RRESP 1, SUPPORTS_NARROW_BURST 0, NUM_READ_OUTSTANDING 1, NUM_WRITE_OUTSTANDING 1, MAX_BURST_LENGTH 1, PHASE 0.0, NUM_READ_THREADS 1, NUM_WRITE_THREADS 1, RUSER_BITS_PER_BYTE 0, WUSER_BITS_PER_BYTE 0, INSERT_VIP 0";
+  attribute x_interface_info of s00_axi_awprot : signal is "xilinx.com:interface:aximm:1.0 s00_axi AWPROT";
+  attribute x_interface_info of s00_axi_awvalid : signal is "xilinx.com:interface:aximm:1.0 s00_axi AWVALID";
+  attribute x_interface_info of s00_axi_awready : signal is "xilinx.com:interface:aximm:1.0 s00_axi AWREADY";
+  attribute x_interface_info of s00_axi_wdata : signal is "xilinx.com:interface:aximm:1.0 s00_axi WDATA";
+  attribute x_interface_info of s00_axi_wstrb : signal is "xilinx.com:interface:aximm:1.0 s00_axi WSTRB";
+  attribute x_interface_info of s00_axi_wvalid : signal is "xilinx.com:interface:aximm:1.0 s00_axi WVALID";
+  attribute x_interface_info of s00_axi_wready : signal is "xilinx.com:interface:aximm:1.0 s00_axi WREADY";
+  attribute x_interface_info of s00_axi_bresp : signal is "xilinx.com:interface:aximm:1.0 s00_axi BRESP";
+  attribute x_interface_info of s00_axi_bvalid : signal is "xilinx.com:interface:aximm:1.0 s00_axi BVALID";
+  attribute x_interface_info of s00_axi_bready : signal is "xilinx.com:interface:aximm:1.0 s00_axi BREADY";
+  attribute x_interface_info of s00_axi_araddr : signal is "xilinx.com:interface:aximm:1.0 s00_axi ARADDR";
+  attribute x_interface_info of s00_axi_arprot : signal is "xilinx.com:interface:aximm:1.0 s00_axi ARPROT";
+  attribute x_interface_info of s00_axi_arvalid : signal is "xilinx.com:interface:aximm:1.0 s00_axi ARVALID";
+  attribute x_interface_info of s00_axi_arready : signal is "xilinx.com:interface:aximm:1.0 s00_axi ARREADY";
+  attribute x_interface_info of s00_axi_rdata : signal is "xilinx.com:interface:aximm:1.0 s00_axi RDATA";
+  attribute x_interface_info of s00_axi_rresp : signal is "xilinx.com:interface:aximm:1.0 s00_axi RRESP";
+  attribute x_interface_info of s00_axi_rvalid : signal is "xilinx.com:interface:aximm:1.0 s00_axi RVALID";
+  attribute x_interface_info of s00_axi_rready : signal is "xilinx.com:interface:aximm:1.0 s00_axi RREADY";
+  attribute x_interface_info of rst_0_port_out : signal is "xilinx.com:signal:reset:1.0 RST.RST_0 RST";
+  attribute x_interface_parameter of rst_0_port_out : signal is "XIL_INTERFACENAME RST.RST_0, INSERT_VIP 0, POLARITY ACTIVE_LOW";
+  attribute x_core_info : string;
+  attribute x_core_info of stub : architecture is "myip_pong2,Vivado 2024.2";
+begin
+end;
