7f44c5a85d359a3f60fc5ce6dfd2e938c46cf102
alexillo1 <santiagorod.alex@ufl.edu>
2025-04-23 01:28:44 -0400

    vivado project files

---
+++ vivado_project/project_2.tmp/myip_pong2_v1_0_project/myip_pong2_v1_0_project.gen/sources_1/ip/design_6_util_vector_logic_0_0/design_6_util_vector_logic_0_0_bmstub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+// -------------------------------------------------------------------------------
+
+`timescale 1 ps / 1 ps
+
+(* BLOCK_STUB = "true" *)
+module design_6_util_vector_logic_0_0 (
+  Op1,
+  Op2,
+  Res
+);
+
+  (* X_INTERFACE_IGNORE = "true" *)
+  input [0:0]Op1;
+  (* X_INTERFACE_IGNORE = "true" *)
+  input [0:0]Op2;
+  (* X_INTERFACE_IGNORE = "true" *)
+  output [0:0]Res;
+
+  // stub module has no contents
+
+endmodule
